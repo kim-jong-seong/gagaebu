85a05bdd640a620610daae41280f6b1b72a9c048
kim-Jong-Seong <74376741+kim-jong-seong@users.noreply.github.com>
2026-03-18 17:40:54 +0900

    Update Node.js interpreter version in config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'gagaebu',
       script: 'src/app.js',
-      interpreter: '/home/ec2-user/.nvm/versions/node/v20.19.0/bin/node',
+      interpreter: '/home/ec2-user/.nvm/versions/node/v20.20.1/bin/node',
       cwd: '/home/ec2-user/gagaebu',
       instances: 1,
       autorestart: true,
